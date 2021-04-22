a55a564d81accd8544d3feefbcdd8d71d4ae9e4a
Dmitriy <dmitriy.babich0@gmail.com>
2021-04-21 17:34:05 -0700

    the color of the time blocks changes with time now

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,32 @@
 var saveBtn = $('.saveBtn');
 var today = $('#currentDay');
 var date = moment().format('MMMM Do YYYY');
+var currentTime = parseInt(moment().format('H'));
+var timeBlock = $('.time-block');
 
 //display current day
 today.text(date);
 
+timeBlock.each(function() {
+    //gets the id value of each time block
+    var idVal = parseInt($(this).attr('id'));
+    
+    //color changing based on what time it is 
+    if (currentTime === idVal) {
+        $(this).children('textarea').attr('class','col-sm-10 description present')
+    } else if (currentTime > idVal) {
+        $(this).children('textarea').attr('class','col-sm-10 description past')
+    } else {
+        $(this).children('textarea').attr('class','col-sm-10 description future')
+    }
+    });
+
+
+
+
+
+
+//if hour = day.value
+// set that day block to green
+// set the block before it to red 
+// set the other blocks to grey
